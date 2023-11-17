8117d23afb9f8a252c426a7575a4dec7b5e6d984
Lilliam <lilliam.harobernal@gmail.com>
2023-11-17 01:12:32 -0500

    start

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import reactLogo from './assets/react.svg'
+import './assets/styles/main.scss'
+
+function App() {
+  return (
+    <>
+    <div className='c-home'>
+      <h2 className='c-home__title'>Tituloss</h2>
+    </div>
+    </>
+  )
+}
+
+export default App
